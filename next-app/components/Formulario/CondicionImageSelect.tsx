6c2d3d20e8dc1b4050a2a52ae64c526b11535210
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-01-29 12:21:10 -0300

    condition graph

--- next-app/components/Formulario/CondicionImageSelect.tsx
+++ next-app/components/Formulario/CondicionImageSelect.tsx
@@ -13,17 +13,16 @@ export default function CondicionImageSelect(props: CondicionImageSelectProps) {
     const { graficoPeso, especie, sexo, update } = props
     const especieSexada = sexo === SEXOS.MACHO ? especie : especie.replace("o", "a")
 
-    const esPerro = especie === ESPECIES.PERRO
+    const folderName = especie === ESPECIES.PERRO ? "DogProfiles" : "CatProfiles"
 
     const handleSelectOption = (e: ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value)
         update({ graficoPeso: parseInt(e.target.value) })
     }
   return (
     <div>
         <label htmlFor="graficoPeso">En que numero te parece que esta tu {especieSexada}</label>
-        <div className="flex items-center justify-evenly p-4">
-            <label htmlFor="valor1" className={`p-4 w-1/5 h-52 relative ${graficoPeso === 1 ? "border-4" : "border-none"}`}>
+        <div className="flex items-center p-4 g-4">
+            <label htmlFor="valor1" className={`p-4 min-w-[126px] max-w-1/5 h-[136px] relative ${graficoPeso === 1 ? "border-4" : "border-none"}`}>
                 <input 
                     type="radio"
                     id="valor1"
@@ -34,13 +33,13 @@ export default function CondicionImageSelect(props: CondicionImageSelectProps) {
                     onChange={handleSelectOption}
                 />
                 <Image 
-                    src="/svgs/dog-food.svg"
+                    src={`/profiles/${folderName}/1.jpeg`}
                     fill
                     alt="se le marcan las caderas y costillas" 
                     className="cursor-pointer"
                 />
             </label>
-            <label htmlFor="valor2" className={`p-4 w-1/5 h-52 relative ${graficoPeso === 2 ? "border-4" : "border-none"}`}>
+            <label htmlFor="valor2" className={`p-4 min-w-[126px] max-w-1/5 h-[136px] relative ${graficoPeso === 2 ? "border-4" : "border-none"}`}>
             <input 
                     type="radio"
                     id="valor2"
@@ -51,13 +50,13 @@ export default function CondicionImageSelect(props: CondicionImageSelectProps) {
                     onChange={handleSelectOption}
                 />
                 <Image 
-                    src="/svgs/dog-food.svg"
+                    src={`/profiles/${folderName}/2.jpeg`}
                     fill
                     alt="se le marcan las caderas y costillas" 
                     className="cursor-pointer"
                 />
             </label>
-            <label htmlFor="valor3" className={`p-4 w-1/5 h-52 relative ${graficoPeso === 3 ? "border-4" : "border-none"}`}>
+            <label htmlFor="valor3" className={`p-4 min-w-[126px] max-w-1/5 h-[136px] relative ${graficoPeso === 3 ? "border-4" : "border-none"}`}>
             <input 
                     type="radio"
                     id="valor3"
@@ -68,13 +67,13 @@ export default function CondicionImageSelect(props: CondicionImageSelectProps) {
                     onChange={handleSelectOption}
                 />
                 <Image 
-                    src="/svgs/dog-food.svg"
+                    src={`/profiles/${folderName}/3.jpeg`}
                     fill
                     alt="se le marcan las caderas y costillas" 
                     className="cursor-pointer"
                 />
             </label>
-            <label htmlFor="valor4" className={`p-4 w-1/5 h-52 relative ${graficoPeso === 4 ? "border-4" : "border-none"}`}>
+            <label htmlFor="valor4" className={`p-4 min-w-[126px] max-w-1/5 h-[136px] relative ${graficoPeso === 4 ? "border-4" : "border-none"}`}>
             <input 
                     type="radio"
                     id="valor4"
@@ -85,13 +84,13 @@ export default function CondicionImageSelect(props: CondicionImageSelectProps) {
                     onChange={handleSelectOption}
                 />
                 <Image 
-                    src="/svgs/dog-food.svg"
+                    src={`/profiles/${folderName}/4.jpeg`}
                     fill
                     alt="se le marcan las caderas y costillas" 
                     className="cursor-pointer"
                 />
             </label>
-            <label htmlFor="valor5" className={`p-4 w-1/5 h-52 relative ${graficoPeso === 5 ? "border-4" : "border-none"}`}>
+            <label htmlFor="valor5" className={`p-4 min-w-[126px] max-w-1/5 h-[136px] relative ${graficoPeso === 5 ? "border-4" : "border-none"}`}>
             <input 
                     type="radio"
                     id="valor5"
@@ -102,7 +101,7 @@ export default function CondicionImageSelect(props: CondicionImageSelectProps) {
                     onChange={handleSelectOption}
                 />
                 <Image 
-                    src="/svgs/dog-food.svg"
+                    src={`/profiles/${folderName}/5.jpeg`}
                     fill
                     alt="se le marcan las caderas y costillas" 
                     className="cursor-pointer"
