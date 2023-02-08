f2aced50aa74662cf420e7cf4fd68cbaa0273e3f
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-02-08 16:49:48 -0300

    setup testimonios

---
+++ next-app/components/Testimonios.tsx
@@ -0,0 +1,12 @@
+// TODO: Setup a carrousel with automatic scroll and option to move forwrads and backwards
+// TODO: Consume testimonios_data and display nicely that info on the carrousel
+
+export default function Testimonios() {
+  return (
+    <div>
+      <div>{'<'}</div>
+      <div></div>
+      <div>{'>'}</div>
+    </div>
+  )
+}
---
+++ next-app/components/testimonios_data.ts
@@ -0,0 +1,74 @@
+interface Testimonio {
+  tutor: string
+  paciente: string
+  texto: string
+}
+
+export const data: Testimonio[] = [
+  {
+    tutor: 'María Pía',
+    paciente: 'Floky',
+    texto:
+      'Verónica es una profesional muy seria, responsable, amorosa y comprensiva. Responde todas las consultas y es súper atenta. Muy recomendable su servicio! Estoy muy agradecida. Y para todas las cosas de mi perrito, en lo clínico y nutricional, lo consulto con ella.'
+  },
+  {
+    tutor: 'Marta Copola',
+    paciente: 'Tom',
+    texto:
+      'A Tom nunca le gusto el balanceado, asi que empezamos con dieta casera cocinada hecha por la doc Veronica. Al poco tiempo ya note los cambios! Comia con ganas, hasta pedia comida!!!! Logro subir de peso y ganar musculo. Juega mucho mas. Es otro perro!! En casa estamos felices con este cambio. Muchas gracias vet!!!!!'
+  },
+  {
+    tutor: 'Laura',
+    paciente: 'Nina',
+    texto:
+      'Respecto a la alimentación natural, no solo se ve en los análisis clínicos de Nina la mejora que tuvo su salud en sólo 2 meses, sino que es hermoso verla entusiasmada cuando come, incluso si es el balanceado con algún complemento. En lo que es atención tuya, no tengo mas que agradecimientos por la paciencia ante todas mis preguntas y lo mas importante, la falta de juicios por no haber dado alimentación natural, entendiendo que muchas veces tratamos de hacer lo mejor podemos por nuestros peludos, dentro de nuestras realidades. Gracias de parte de Nina y Laura'
+  },
+  {
+    tutor: 'Mariela',
+    paciente: 'Atena',
+    texto:
+      'Atena consume comida balanceada Se la cambie por una mejor y le incorporé alimentación de otro tipo (comida real) para mejorar su alimentación sin q engorde. Fueron todas sugerencias y le vino muy bien. Estuvo bueno averiguar y sacarse muchas dudas'
+  },
+  {
+    tutor: 'Jazmin',
+    paciente: 'Moana',
+    texto:
+      ' La realidad es que después de realizar el cambio de alimentación a natural cambió completamente el humor de ella al momento de comer. Nunca le gustó su balanceado, y ahora AMA el momento de la comida. Verla comer con ganas es otro mundo. Con respecto a la asesoría, fue súper práctica y me encantó el material enviado, está toda la información que se puede necesitar ante cualquier duda. Desde la transición, formulación de la dieta hasta recetas y alimentos prohibidos. Al día de hoy sigo en contacto con Vero por algunas dudas y nunca dejó de responderme, siempre me contesta las dudas que tengo. Lo mismo en el mes y medio de transición que tuvimos, una genia. La voy a recomendar siempre ❤️'
+  },
+  {
+    tutor: 'Felipe',
+    paciente: 'William',
+    texto:
+      'Verónica es súper detallista y muy atenta a la escucha de tus necesidades. Siempre está a disposición ante cada duda en este proceso de mejora de la vida de nuestras mascotas. Súper recomendable.'
+  },
+  {
+    tutor: 'Laura',
+    paciente: 'Imperial y Alem',
+    texto:
+      'Imperial y Alem tenían 3 meses aprox cuando opte por aprender sobre su alimentación. Y de esa manera buscar alimentos que sirvan para su crecimiento y vida. Ahora tienen 6 meses y están súper bien. Hago comparación con otros cachorros y se nota la diferencia es aspecto, tamaño y energia. Hubo alimentos que consumieron súper bien y otros un poco menos. Pero estamos aún en proceso de aprendizaje.'
+  },
+  {
+    tutor: 'Gabriela',
+    paciente: 'Demon',
+    texto:
+      'Al principio me complique mucho pero con el pasar de los dias me di cuenta que es solo costumbre y preparacion nada más, una vez q esta todo cocido solo calentas en el micro, pones las proporciones y listo.'
+  },
+  {
+    tutor: 'Karina',
+    paciente: 'Nina y Pipa',
+    texto:
+      'Me fue muy bien con la acesoria y tambien ya la recomende con amigas. Muchas gracias!!'
+  },
+  {
+    tutor: 'Micaela',
+    paciente: 'Nala y Ragnar',
+    texto:
+      'La verdad que estoy muy contenta con la alimentación que le estoy dando ya que noto que les cambio el pelaje, inclusive el aliento y se nota que aman lo que comen ahora!'
+  },
+  {
+    tutor: 'Gre',
+    paciente: 'Toto',
+    texto:
+      'Recien estamos empezando y organizando todo. Siempre le he cocinado y dado alimento casero como complemento, pero ahora que toti esta mas viejo estaba mas mañoso. Las asesorias nutricionales son EXCELENTES. Son super completas, aptas para que podamos entenderlas y aplicarlas. Aprendi cosas que no sabia y estaba haciendo mal. A toto se nota que le gusta y esperemos poder ganar masa muscular para sus problemas articulares'
+  }
+]
