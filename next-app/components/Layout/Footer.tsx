74bfc719a78d21244ed38053ab928be074d1300b
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-02-10 16:52:28 -0300

    footer min style

--- next-app/components/Layout/Footer.tsx
+++ next-app/components/Layout/Footer.tsx
@@ -1,5 +1,10 @@
 // TODO: Complete
 
 export default function Footer() {
-  return <footer className="hidden sm:block">Links y eso aqui</footer>
+  return (
+    <footer className="to-terciary from-terciary/10 text-primary mt-8 hidden h-24 w-full flex-col items-center justify-start bg-gradient-to-b p-6 sm:flex">
+      <p>Veronica Roffo - Veterinaria MN 10468</p>
+      <p>&copy; 2023 - Buenos Aires, Argentina</p>
+    </footer>
+  )
 }
