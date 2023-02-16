c703a929f64335b3dfdbf31cffe29f51ed8a6fd5
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-02-16 17:56:53 -0300

    about page minimal data

--- next-app/components/Layout/Footer.tsx
+++ next-app/components/Layout/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 // TODO: Complete
 
 export default function Footer() {
@@ -5,6 +6,7 @@ export default function Footer() {
     <footer className="to-terciary from-terciary/10 text-primary mt-8 hidden h-24 w-full flex-col items-center justify-start bg-gradient-to-b p-6 sm:flex">
       <p>Veronica Roffo - Veterinaria MN 10468</p>
       <p>&copy; 2023 - Buenos Aires, Argentina</p>
+      <Link href="/report">Report a problem!</Link>
     </footer>
   )
 }
--- next-app/pages/about.tsx
+++ next-app/pages/about.tsx
@@ -1,13 +1,60 @@
 import Layout from '@/components/Layout/Layout'
+import Container from '@/components/Container'
 
-// TODO: Complete
+// TODO: agregar estilos
 
-export default function about() {
+export default function About() {
   return (
     <Layout title="Sobre mi">
-      <h1 className="text-primary text-center text-6xl font-bold">
-        Vero, completame!
-      </h1>
+      <h1 className="text-primary text-center text-6xl font-bold">Sobre mi</h1>
+      <Container>
+        <div>
+          <h3>Mi nombre es Veronica Roffo</h3>
+          {/*TODO: foto */}
+        </div>
+        <p>
+          Me recibí en el año 2019 de Veterinaria en la Universidad de Buenos
+          Aires y desde entonces trabajo apasionadamente con animales grandes y
+          pequeños
+        </p>
+        <p>
+          Me especializo en nutricion de felinos y caninos, atendiendo casos
+          clinicos y brindando soluciones para mejorar las dietas de mis
+          pacientes
+        </p>
+        <p>
+          Ademas de las asesorias realizo divulgacion de buenas practicas y
+          recomendaciones para la nutricion animal en{' '}
+          <a
+            href="https://www.instagram.com/vr.veterinaria/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            mi instagram
+          </a>
+        </p>
+        <p>
+          Para estar siempre actualizada en este tema que tanto me apasiona, he
+          realizado y realizo muchos cursos y capacitaciones. Entre ellos:
+          <ul>
+            {/*TODO: Agregar links a las pags de los cursos? Certificados?*/}
+            <li>
+              Máster “Alimentación natural y nutrición funcional para perros y
+              gatos” - Biovet
+            </li>
+            <li>
+              Curso “Nutrición natural en caninos y felinos domésticos” - Red
+              Animal Chile
+            </li>
+            <li>
+              Curso “Nutrición natural de mascotas y taller de patologías” -
+              Christian Vergara
+            </li>
+            {/* Mas?? */}
+          </ul>
+        </p>
+        <p></p>
+      </Container>
     </Layout>
   )
 }
